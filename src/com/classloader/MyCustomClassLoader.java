94c1fb7b1be914ea8dc5eff4ab04186a6b844705
liushangqing <378397197@qq.com>
2021-11-03 00:00:05 +0800

    调试通过

--- src/com/classloader/MyCustomClassLoader.java
+++ src/com/classloader/MyCustomClassLoader.java
@@ -1,12 +1,16 @@
 package com.classloader;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.nio.ByteBuffer;
 import java.nio.MappedByteBuffer;
+import java.nio.channels.Channels;
 import java.nio.channels.FileChannel;
+import java.nio.channels.WritableByteChannel;
 
 /**
  * @author liushangqing
@@ -14,6 +18,36 @@ import java.nio.channels.FileChannel;
  * 自定义类加载器
  */
 public class MyCustomClassLoader extends ClassLoader {
+    private String classPath;
+    public MyCustomClassLoader( String classPath) {
+        this.classPath = classPath;
+    }
+
+    private byte[] getClassByte(String name) throws IOException {
+        String classFile = classPath + File.separator + name.replace(".", File.separator)+".class";
+        System.out.println(classFile);
+        File file = new File(classFile);
+        FileInputStream fileInputStream = new FileInputStream(file);
+        FileChannel fileChannel = fileInputStream.getChannel();
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        WritableByteChannel writableByteChannel = Channels.newChannel(byteArrayOutputStream);
+        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+        int i;
+        while (true) {
+            i = fileChannel.read(byteBuffer);
+            if (i == 0 || i == -1) {
+                break;
+            }
+            byteBuffer.flip();
+            writableByteChannel.write(byteBuffer);
+            byteBuffer.clear();
+        }
+        writableByteChannel.close();
+        fileChannel.close();
+        fileInputStream.close();
+        return byteArrayOutputStream.toByteArray();
+    }
+
     /**
      * 重写父类方法,得到一個Class对象
      *
@@ -24,13 +58,13 @@ public class MyCustomClassLoader extends ClassLoader {
     @Override
     protected Class<?> findClass(String name) throws ClassNotFoundException {
         Class clazz = null;
-        String classFilename = name + ".class";
+        String classFilename = classPath+File.separator + name.replace(".", File.separator)+".class";
+        System.out.println(classFilename);
         File classFile = new File(classFilename);
         if (classFile.exists()) {
-            try (FileChannel fileChannel = new FileInputStream(classFile).getChannel()) {
-                MappedByteBuffer mappedByteBuffer = fileChannel.map(FileChannel.MapMode.READ_ONLY, 0, fileChannel.size());
-                byte[] array = mappedByteBuffer.array();
-                clazz = defineClass(name, array, 0, array.length);
+            try {
+                byte[] bytes = getClassByte(name);
+                clazz = defineClass(name, bytes, 0, bytes.length);
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -41,10 +75,11 @@ public class MyCustomClassLoader extends ClassLoader {
         return clazz;
     }
 
-    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-        MyCustomClassLoader myCustomClassLoader = new MyCustomClassLoader();
-        Class<?> aClass = myCustomClassLoader.loadClass(args[0]);
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException, InstantiationException {
+        MyCustomClassLoader myCustomClassLoader = new MyCustomClassLoader("E:\\learn\\jvm-learn\\target");
+        Class<?> aClass = myCustomClassLoader.findClass("com.classloader.Hello");
+        Object object = aClass.newInstance();
         Method hello = aClass.getMethod("hello");
-        hello.invoke(null, null);
+        hello.invoke(object);
     }
 }
